76b0c76d2eb92ff91c0d1fd28199ff4353422cfe
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-18 05:06:37 +0000

    CircleAvatar & Icons

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -20,58 +22,88 @@ class Profile extends StatelessWidget {
             horizontal: 10,
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 30,
+                height: 50,
               ),
               Container(
-                height: 80,
+                height: 50,
                 color: Colors.white,
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "4:20",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                        fontSize: 18,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: Row(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "4:20",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Icon(Icons.network_cell),
-                    Icon(Icons.wifi),
-                    Icon(Icons.battery_full),
-                    Row()
-                    // SizedBox(
-                    //   height: 30,
-                    // ),
-                    // CircleAvatar(
-                    //   backgroundImage: AssetImage('images/2.jpg'),
-                    // ),
-                    // SizedBox(
-                    //   width: 10,
-                    // ),
-                    // Icon(Icons.phone),
-                    // SizedBox(
-                    //   width: 10,
-                    // ),
-                    // Text(
-                    //   "Pro",
-                    //   style: TextStyle(
-                    //     fontWeight: FontWeight.bold,
-                    //     color: Colors.black87,
-                    //   ),
-                    // ),
-                    // Spacer(),
-                    // Icon(Icons.notifications),
-
-                    // Icon(
-                    //   Icons.fiber_manual_record_outlined,
-                    //   size: 8,
-                    // ),
-                  ],
+                      Spacer(),
+                      Icon(
+                        Icons.network_cell,
+                        size: 18,
+                      ),
+                      Icon(
+                        Icons.wifi,
+                        size: 18,
+                      ),
+                      Icon(
+                        Icons.battery_full,
+                        size: 18,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                height: 70,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(
+                          'images/2.jpg',
+                        ),
+                        radius: 20,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.school,
+                        color: Colors.blueAccent,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Pro",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                          fontSize: 15,
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.notification_add,
+                        size: 20,
+                        color: Colors.blueAccent,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
