401c21b3823911ee36357698428960cc926f377a
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-18 17:39:22 +0000

    completed second container

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
@@ -109,60 +111,210 @@ class Profile extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              // Second Container open
+              
+              
+              // Second Container open!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
               Container(
-                height: 200,
-                color: Colors.red,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Row(
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Savings Balance",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black54,
-                          fontSize: 15,
+                width: 372,
+                height: 180,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  color: Colors.white,
+                  elevation: 10,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 12,
                         ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.visibility_off_outlined,
-                        size: 18,
-                      ),
-                      Spacer(),
-                      Container(
-                        height: 30,
-                        width: 65,
-                        color: Colors.green,
-                        child: Row(
-                          // crossAxisAlignment: CrossAxisAlignment.start,
+                        Row(
                           children: [
-                            RichText(
-                              text: TextSpan(
-                                text: "+300",
-                                style: TextStyle(
-                                  fontSize: 15,
+                            Text(
+                              "Savings Balance",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black38,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Icon(
+                              Icons.visibility_off_outlined,
+                              size: 16,
+                            ),
+                            Spacer(
+                              flex: 1,
+                            ),
+                            Container(
+                              width: 80,
+                              height: 30,
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                                children: [
-                                  TextSpan(
-                                    text: 'CFA',
-                                    style: TextStyle(
-                                        fontSize: 15, color: Colors.white38),
+                                color: Colors.greenAccent,
+                                elevation: 10,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: Row(
+                                    children: [
+                                      RichText(
+                                        text: TextSpan(
+                                          text: '+300',
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          children: [
+                                            TextSpan(
+                                              text: 'CFA',
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.black26,
+                                                // fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
+                                ),
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
+                        RichText(
+                          text: TextSpan(
+                            text: "150,000",
+                            style: TextStyle(
+                              fontSize: 27,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "CFA",
+                                style: TextStyle(
+                                  fontSize: 27,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            InkWell(
+                              onTap: () {},
+                              child: Container(
+                                width: 150,
+                                height: 45,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  color: Colors.blueAccent,
+                                  elevation: 10,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 30),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          FontAwesomeIcons.plus,
+                                          color: Colors.white,
+                                          size: 15,
+                                        ),
+                                        SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          'Save',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            InkWell(
+                              onTap: () {},
+                              child: Container(
+                                width: 150,
+                                height: 45,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  color: Colors.teal.shade50,
+                                  elevation: 10,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 25),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          FontAwesomeIcons.minus,
+                                          color: Colors.blueAccent,
+                                          size: 15,
+                                        ),
+                                        SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          'Withdraw',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.blueAccent,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "The amount is the net sum of all your savings",
+                          style: TextStyle(
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+              ),
+// Second Container Closed!!!!!!!!!!!!!!!!!
+              Text("The amount is the net sum of all your savings"),
+              Container(
+                color: Colors.blue,
+                width: 50,
               ),
             ],
           ),
